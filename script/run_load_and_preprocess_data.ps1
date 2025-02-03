783732d209d3fa336c128055bcfd5ab58e547f23
Valarezo Plaza <qpw475@usask.ca>
2025-02-03 17:12:48 -0600

    script to run the python file, using arguments to easy access in windows

---
+++ script/run_load_and_preprocess_data.ps1
@@ -0,0 +1,36 @@
+param (
+    [string]$dataRoot,
+    [string]$projectRoot,
+    [string]$windowSize,
+    [string]$stabilityFile,
+    [string]$approach
+)
+
+# Assign default values to inputFolder and rmsFile if they are not provided
+if (-not $dataRoot) {
+    $dataRoot = "C:\Users\qpw475\Documents\combustion_instability\data\raw\h2"
+}
+
+if (-not $projectRoot) {
+    $projectRoot = "C:\Users\qpw475\Documents\combustion_instability"
+}
+
+if (-not $stabilityFile) {
+    $stabilityFile = "C:\Users\qpw475\Documents\combustion_instability\data\labels\h2_label.csv"
+}
+
+if (-not $windowSize) {
+    $windowSize = "100"
+}
+
+if (-not $approach) {
+    $approach = "time_series"
+}
+
+
+
+$python_executable = "C:\Programs\Anaconda3\envs\hidrogen\python.exe"
+$python_script = "C:\Users\qpw475\Documents\combustion_instability\src\load_and_preprocess_data.py"
+
+# Construct the command to run the Python script
+& $python_executable $python_script --data_root $dataRoot --project_root $projectRoot --stability_file $stabilityFile --window_size $windowSize --approach $approach
