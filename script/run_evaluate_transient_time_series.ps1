2ece4cb9f60f8da2469d950217277c0503246c1b
Valarezo Plaza <qpw475@usask.ca>
2025-02-05 12:07:47 -0600

    Script to run python file to evaluate the model with the transient data in time series approach.

---
+++ script/run_evaluate_transient_time_series.ps1
@@ -0,0 +1,40 @@
+param (
+    [string]$dataRoot,
+    [string]$projectRoot,
+    [int]$windowSize,
+    [string]$stabilityFile,
+    [string]$approach,
+    [int]$duration_sample_ms
+)
+
+# Assign default values to inputFolder and rmsFile if they are not provided
+if (-not $dataRoot) {
+    $dataRoot = "C:\Users\qpw475\Documents\combustion_instability\data\raw\h2"
+}
+
+if (-not $projectRoot) {
+    $projectRoot = "C:\Users\qpw475\Documents\combustion_instability"
+}
+
+if (-not $stabilityFile) {
+    $stabilityFile = "C:\Users\qpw475\Documents\combustion_instability\data\labels\h2_label.csv"
+}
+
+if (-not $windowSize) {
+    $windowSize = 300
+}
+
+if (-not $duration_sample_ms) {
+    $duration_sample_ms = 24000
+}
+
+if (-not $approach) {
+    $approach = "time_series"
+}
+
+
+
+$python_executable = "C:\Programs\Anaconda3\envs\hidrogen\python.exe"
+$python_script = "C:\Users\qpw475\Documents\combustion_instability\src\evaluate_transient_time_series_model.py"
+# Construct the command to run the Python script
+& $python_executable $python_script --data_root $dataRoot --project_root $projectRoot --stability_file $stabilityFile --window_size $windowSize --approach $approach --duration_sample_ms $duration_sample_ms
