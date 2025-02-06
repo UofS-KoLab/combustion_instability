7a186ebc349469bc7f0a539bc7f6e172e7c07c44
Valarezo Plaza <qpw475@usask.ca>
2025-02-05 20:32:14 -0600

    Run script evaluation time series normal.

---
+++ script/run_evaluate_normal_time_series.ps1
@@ -0,0 +1,35 @@
+param (
+    [string]$dataRoot,
+    [string]$projectRoot,
+    [int]$windowSize,
+    [string]$approach,
+    [string]$modelName
+)
+
+# Assign default values to inputFolder and rmsFile if they are not provided
+if (-not $dataRoot) {
+    $dataRoot = "C:\Users\qpw475\Documents\combustion_instability\data\raw\h2"
+}
+
+if (-not $projectRoot) {
+    $projectRoot = "C:\Users\qpw475\Documents\combustion_instability"
+}
+
+if (-not $windowSize) {
+    $windowSize = 100
+}
+
+if (-not $approach) {
+    $approach = "time_series"
+}
+
+if (-not $modelName) {
+    $modelName = "model_4"
+}
+
+
+
+$python_executable = "C:\Programs\Anaconda3\envs\hidrogen\python.exe"
+$python_script = "C:\Users\qpw475\Documents\combustion_instability\src\evaluate_normal_time_series_model.py"
+# Construct the command to run the Python script
+& $python_executable $python_script --data_root $dataRoot --project_root $projectRoot --window_size $windowSize --approach $approach --model_name $modelName
